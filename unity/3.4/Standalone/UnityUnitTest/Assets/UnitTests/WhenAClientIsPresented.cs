85397b46b3323376d7c2fe01eedb8218105c2188
crimsonred <crimsonred10@gmail.com>
2013-07-03 15:23:52 +0530

    Modified the pubnub example to fix the hang issues

--- unity/3.4/Standalone/UnityUnitTest/Assets/UnitTests/WhenAClientIsPresented.cs
+++ unity/3.4/Standalone/UnityUnitTest/Assets/UnitTests/WhenAClientIsPresented.cs
@@ -7,20 +7,12 @@ using System.ComponentModel;
 using System.Threading;
 using System.Collections;
 using PubNubMessaging.Core;
-#if (USE_JSONFX)
-using JsonFx.Json;
-#elif (USE_DOTNET_SERIALIZATION)
-using System.Runtime.Serialization.Json;
-using System.Web.Script.Serialization;
-#else
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-#endif
+
 namespace PubNubMessaging.Tests
 {
     public class WhenAClientIsPresented: UUnitTestCase
     {
-        /*ManualResetEvent subscribeManualEvent = new ManualResetEvent(false);
+        ManualResetEvent subscribeManualEvent = new ManualResetEvent(false);
         ManualResetEvent presenceManualEvent = new ManualResetEvent(false);
         ManualResetEvent unsubscribeManualEvent = new ManualResetEvent(false);
 
@@ -34,7 +26,7 @@ namespace PubNubMessaging.Tests
 
         static bool receivedPresenceMessage = false;
         static bool receivedHereNowMessage = false;
-        static bool receivedCustomUUID = false;*/
+        static bool receivedCustomUUID = false;
 
         string customUUID = "mylocalmachine.mydomain.com";
 
@@ -42,224 +34,255 @@ namespace PubNubMessaging.Tests
         public void ThenPresenceShouldReturnReceivedMessage()
         {
 			Debug.Log("Running ThenPresenceShouldReturnReceivedMessage()");
-			Pubnub pubnub = new Pubnub(
-				"demo",
-				"demo",
-				"",
-				"",
-				false
-				);
-			string channel = "hello_world";
-			Common commonPresence = new Common();
-			commonPresence.DeliveryStatus = false;
-			commonPresence.Response = null;
-			
-			pubnub.PubnubUnitTest = commonPresence.CreateUnitTestInstance("WhenAClientIsPresented", "ThenPresenceShouldReturnReceivedMessage");
-			
-			pubnub.Presence(channel, commonPresence.DisplayReturnMessageDummy, commonPresence.DisplayReturnMessageDummy, commonPresence.DisplayReturnMessageDummy);
-			
-			Common commonSubscribe = new Common();
-			commonSubscribe.DeliveryStatus = false;
-			commonSubscribe.Response = null;
-			
-			pubnub.Subscribe(channel, commonSubscribe.DisplayReturnMessage, commonSubscribe.DisplayReturnMessageDummy, commonPresence.DisplayReturnMessageDummy);
-			while (!commonSubscribe.DeliveryStatus) ;
-			
-			string response = "";
-			if (commonSubscribe.Response == null) {
-				Debug.Log("Null response");
-				UUnitAssert.Fail();
-			}
-			else
-			{
-				IList<object> responseFields = commonSubscribe.Response as IList<object>;
-				foreach (object item in responseFields)
-				{
-					response = item.ToString();
-					Console.WriteLine("Response:" + response);
-					//Assert.IsNotEmpty(strResponse);
-				}
-				bool result = "hello_world".Equals(responseFields[2]);
-				UUnitAssert.True(result);
-				Debug.Log("ThenPresenceShouldReturnReceivedMessage: " + result.ToString());
-			}
+            receivedPresenceMessage = false;
+
+            Pubnub pubnub = new Pubnub("demo", "demo", "", "", false);
+
+            PubnubUnitTest unitTest = new PubnubUnitTest();
+            unitTest.TestClassName = "WhenAClientIsPresented";
+            unitTest.TestCaseName = "ThenPresenceShouldReturnReceivedMessage";
+            pubnub.PubnubUnitTest = unitTest;
+            
+            string channel = "hello_my_channel";
+
+            pubnub.Presence<string>(channel, ThenPresenceShouldReturnMessage, PresenceDummyMethodForConnectCallback, DummyErrorCallback);
+            Thread.Sleep(1000);
+            
+            //since presence expects from stimulus from sub/unsub...
+            pubnub.Subscribe<string>(channel, DummyMethodForSubscribe, SubscribeDummyMethodForConnectCallback, DummyErrorCallback);
+            Thread.Sleep(1000);
+            subscribeManualEvent.WaitOne(310 * 1000);
+
+            presenceManualEvent.WaitOne(310 * 1000);
+
+            pubnub.EndPendingRequests();
+            
+            UUnitAssert.True(receivedPresenceMessage, "Presence message not received");
         }
 
         [UUnitTest]
-		public void ThenPresenceShouldReturnCustomUUID ()
-		{
-			Pubnub pubnub = new Pubnub("demo", "demo", "", "", false);
-			
-			Common commonHereNow = new Common();
-			commonHereNow.DeliveryStatus = false;
-			commonHereNow.Response = null;
-			
-			Common commonSubscribe = new Common();
-			commonSubscribe.DeliveryStatus = false;
-			commonSubscribe.Response = null;
-			
-			pubnub.PubnubUnitTest = commonHereNow.CreateUnitTestInstance("WhenAClientIsPresented", "ThenPresenceShouldReturnCustomUUID");;
-			pubnub.SessionUUID = "CustomSessionUUIDTest";
-			
-			string channel = "hello_world";
-			
-			pubnub.Subscribe(channel, commonSubscribe.DisplayReturnMessageDummy, commonSubscribe.DisplayReturnMessage, commonSubscribe.DisplayReturnMessage);
-			
-			while (!commonSubscribe.DeliveryStatus);
-			
-			pubnub.HereNow<string>(channel, commonHereNow.DisplayReturnMessage, commonHereNow.DisplayReturnMessage);
-			
-			while (!commonHereNow.DeliveryStatus);
-			if (commonHereNow.Response!= null)
-			{
-#if (USE_JSONFX)
-				IList<object> fields = new JsonFXDotNet ().DeserializeToObject (commonHereNow.Response.ToString ()) as IList<object>;
-				if (fields [0] != null)
-				{
-					bool result = false;
-					Dictionary<string, object> message = (Dictionary<string, object>)fields [0];
-					foreach (KeyValuePair<String, object> entry in message)
-					{
-						Console.WriteLine("value:" + entry.Value + "  " + "key:" + entry.Key);
-						Type valueType = entry.Value.GetType();
-						var expectedType = typeof(string[]);
-						if (valueType.IsArray && expectedType.IsAssignableFrom(valueType))
-						{
-							List<string> uuids = new List<string>(entry.Value as string[]);
-							if(uuids.Contains(pubnub.SessionUUID )){
-								result= true;
-								break;
-							}
-						}
-					}
-					UUnitAssert.True(result);
-					Debug.Log("ThenPresenceShouldReturnCustomUUID: " + result.ToString());
-				} 
-				else
-				{
-					Debug.Log("Null response");
-					UUnitAssert.Fail();
-				}
-#else
-				object[] serializedMessage = JsonConvert.DeserializeObject<object[]>(commonHereNow.Response.ToString());
-				JContainer dictionary = serializedMessage[0] as JContainer;
-				var uuid = dictionary["uuids"].ToString();
-				if (uuid != null)
-				{
-					Assert.True(uuid.Contains(pubnub.SessionUUID));
-				} else {
-					Assert.Fail("Custom uuid not found.");
-				}
-#endif
-			} else {
-				Debug.Log("Null response");
-				UUnitAssert.Fail();
-			}
-			
-		}
+        public void ThenPresenceShouldReturnCustomUUID()
+        {
+			Debug.Log("Running ThenPresenceShouldReturnCustomUUID()");
+            receivedCustomUUID = false;
 
-        /*[UUnitTest]
+            Pubnub pubnub = new Pubnub("demo", "demo", "", "", false);
+
+            PubnubUnitTest unitTest = new PubnubUnitTest();
+            unitTest.TestClassName = "WhenAClientIsPresented";
+            unitTest.TestCaseName = "ThenPresenceShouldReturnCustomUUID";
+            pubnub.PubnubUnitTest = unitTest;
+
+            string channel = "hello_my_channel";
+
+            pubnub.Presence<string>(channel, ThenPresenceWithCustomUUIDShouldReturnMessage, PresenceUUIDDummyMethodForConnectCallback, DummyErrorCallback);
+            Thread.Sleep(1000);
+            
+            //since presence expects from stimulus from sub/unsub...
+            pubnub.SessionUUID = customUUID;
+            pubnub.Subscribe<string>(channel, DummyMethodForSubscribeUUID, SubscribeUUIDDummyMethodForConnectCallback, DummyErrorCallback);
+            Thread.Sleep(1000);
+            subscribeUUIDManualEvent.WaitOne(310 * 1000);
+
+            presenceUUIDManualEvent.WaitOne(310 * 1000);
+
+            pubnub.EndPendingRequests();
+
+            UUnitAssert.True(receivedCustomUUID, "Custom UUID not received");
+        }
+
+        [UUnitTest]
         public void IfHereNowIsCalledThenItShouldReturnInfo()
         {
 			Debug.Log("Running IfHereNowIsCalledThenItShouldReturnInfo()");
             receivedHereNowMessage = false;
 
             Pubnub pubnub = new Pubnub("demo", "demo", "", "", false);
-			pubnub.JsonPluggableLibrary = new JsonFXDotNet();
 			
             PubnubUnitTest unitTest = new PubnubUnitTest();
             unitTest.TestClassName = "WhenAClientIsPresented";
             unitTest.TestCaseName = "IfHereNowIsCalledThenItShouldReturnInfo";
             pubnub.PubnubUnitTest = unitTest;
-            string channel = "my/channel";
+			
+            string channel = "hello_my_channel";
             pubnub.HereNow<string>(channel, ThenHereNowShouldReturnMessage, DummyErrorCallback);
-            hereNowManualEvent.WaitOne();
+            hereNowManualEvent.WaitOne(310 * 1000);
             UUnitAssert.True(receivedHereNowMessage, "here_now message not received");
-        }*/
+        }
 
-		[UUnitTest]
-		public void IfHereNowIsCalledThenItShouldReturnInfo()
-		{
-			Pubnub pubnub = new Pubnub(
-				"demo",
-				"demo",
-				"",
-				"",
-				false
-				);
-			Common common = new Common();
-			common.DeliveryStatus = false;
-			common.Response = null;
-			
-			HereNow(pubnub, "IfHereNowIsCalledThenItShouldReturnInfo", common.DisplayReturnMessage);
-			while (!common.DeliveryStatus) ;
-			
-			ParseResponse(common.Response);
-		}
-		
-		void HereNow(Pubnub pubnub, string unitTestCaseName, 
-		             Action<object> userCallback)
-		{
-			string channel = "hello_world";
-			
-			PubnubUnitTest unitTest = new PubnubUnitTest();
-			unitTest.TestClassName = "WhenAClientIsPresented";
-			unitTest.TestCaseName = unitTestCaseName;
-			pubnub.PubnubUnitTest = unitTest;
-			
-			pubnub.HereNow(channel, userCallback, userCallback);
-		}
-		
-		public void ParseResponse(object commonResponse)
-		{
-			string response = "";
-			if (commonResponse.Equals (null)) {
-				Debug.Log("Null response");
-				UUnitAssert.Fail();
-			}
-			else
-			{
-				IList<object> responseFields = commonResponse as IList<object>;
-				foreach(object item in responseFields)
-				{
-					response = item.ToString();
-					Console.WriteLine("Response:" + response);
-					bool result = !string.IsNullOrEmpty(response);
-					UUnitAssert.True(result);
-				}
-				Dictionary<string, object> message = (Dictionary<string, object>)responseFields[0];
-				foreach(KeyValuePair<String, object> entry in message)
-				{
-					Console.WriteLine("value:" + entry.Value + "  " + "key:" + entry.Key);
-				}
-				
-				/*object[] objUuid = (object[])message["uuids"];
-                    foreach (object obj in objUuid)
+        void ThenPresenceShouldReturnMessage(string receivedMessage)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(receivedMessage) && !string.IsNullOrEmpty(receivedMessage.Trim()))
+                {
+                    object[] serializedMessage = new JsonFXDotNet().DeserializeToListOfObject(receivedMessage).ToArray();
+                    Dictionary<string,object> dictionary = serializedMessage[0] as Dictionary<string,object>;
+                    var uuid = dictionary["uuid"].ToString();
+                    if (uuid != null)
                     {
-                        Console.WriteLine(obj.ToString()); 
-                    }*/
-				//Assert.AreNotEqual(0, message["occupancy"]);
+                        receivedPresenceMessage = true;
+                    }
+                }
+            }
+            catch{}
+            finally
+            {
+                presenceManualEvent.Set();
+            }
+        }
+
+        void ThenPresenceWithCustomUUIDShouldReturnMessage(string receivedMessage)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(receivedMessage) && !string.IsNullOrEmpty(receivedMessage.Trim()))
+                {
+                    object[] serializedMessage = new JsonFXDotNet().DeserializeToListOfObject(receivedMessage).ToArray();
+                    Dictionary<string,object> dictionary = serializedMessage[0] as Dictionary<string,object>;
+                    var uuid = dictionary["uuid"].ToString();
+                    if (uuid != null && uuid.Contains(customUUID))
+                    {
+                        receivedCustomUUID = true;
+                    }
+                }
+            }
+            catch { }
+            finally
+            {
+                presenceUUIDManualEvent.Set();
+            }
+        }
+
+        void ThenHereNowShouldReturnMessage(string receivedMessage)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(receivedMessage) && !string.IsNullOrEmpty(receivedMessage.Trim()))
+                {
+                    object[] serializedMessage = new JsonFXDotNet().DeserializeToListOfObject(receivedMessage).ToArray();
+                    Dictionary<string,object> dictionary = serializedMessage[0] as Dictionary<string,object>;
+                    if (dictionary != null)
+                    {
+                        receivedHereNowMessage = true;
+                    }
+                }
+            }
+            catch { }
+            finally
+            {
+                hereNowManualEvent.Set();
+            }
+        }
+
+        void DummyMethodForSubscribe(string receivedMessage)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(receivedMessage) && !string.IsNullOrEmpty(receivedMessage.Trim()))
+                {
+                    object[] serializedMessage = new JsonFXDotNet().DeserializeToListOfObject(receivedMessage).ToArray();
+                    Dictionary<string,object> dictionary = serializedMessage[0] as Dictionary<string,object>;
+                    if (dictionary != null)
+                    {
+	                    var uuid = dictionary["uuid"].ToString();
+	                    if (uuid != null)
+	                    {
+	                        receivedPresenceMessage = true;
+	                    }
+					}
+                }
+            }
+            catch(Exception ex) 
+			{ 
+				Debug.Log("DummyMethodForSubscribe exception = " + ex.ToString());
 			}
-		}
-		
-		[UUnitTest]
-		public void IfHereNowIsCalledWithCipherThenItShouldReturnInfo()
-		{
-			Pubnub pubnub = new Pubnub(
-				"demo",
-				"demo",
-				"",
-				"enigma",
-				false
-				);
-			Common common = new Common();
-			common.DeliveryStatus = false;
-			common.Response = null;
-			
-			HereNow(pubnub, "IfHereNowIsCalledThenItShouldReturnInfo", common.DisplayReturnMessage);
-			while (!common.DeliveryStatus) ;
-			
-			ParseResponse(common.Response);
-		}
+            finally
+            {
+                presenceManualEvent.Set();
+            }
+            //Dummary callback method for subscribe and unsubscribe to test presence
+        }
+
+        void DummyMethodForSubscribeUUID(string receivedMessage)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(receivedMessage) && !string.IsNullOrEmpty(receivedMessage.Trim()))
+                {
+                    object[] serializedMessage = new JsonFXDotNet().DeserializeToListOfObject(receivedMessage).ToArray();
+                    Dictionary<string,object> dictionary = serializedMessage[0] as Dictionary<string,object>;
+                    if (dictionary != null)
+                    {
+                        var uuid = dictionary["uuid"].ToString();
+                        if (uuid != null)
+                        {
+                            receivedCustomUUID = true;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) 
+			{ 
+				Debug.Log("DummyMethodForSubscribeUUID exception = " + ex.ToString());
+			}
+            finally
+            {
+                presenceUUIDManualEvent.Set();
+            }
+            //Dummary callback method for subscribe and unsubscribe to test presence
+        }
+
+        void DummyMethodForUnSubscribe(string receivedMessage)
+        {
+            //Dummary callback method for unsubscribe to test presence
+        }
+
+        void DummyMethodForUnSubscribeUUID(string receivedMessage)
+        {
+            //Dummary callback method for unsubscribe to test presence
+        }
+
+        void PresenceDummyMethodForConnectCallback(string receivedMessage)
+        {
+        }
+
+        void PresenceUUIDDummyMethodForConnectCallback(string receivedMessage)
+        {
+        }
+
+        void SubscribeDummyMethodForConnectCallback(string receivedMessage)
+        {
+            subscribeManualEvent.Set();
+        }
+
+        void SubscribeUUIDDummyMethodForConnectCallback(string receivedMessage)
+        {
+            subscribeUUIDManualEvent.Set();
+        }
+
+
+        void UnsubscribeDummyMethodForConnectCallback(string receivedMessage)
+        {
+        }
+
+        void UnsubscribeUUIDDummyMethodForConnectCallback(string receivedMessage)
+        {
+        }
+
+        void UnsubscribeDummyMethodForDisconnectCallback(string receivedMessage)
+        {
+            unsubscribeManualEvent.Set();
+        }
+
+        void UnsubscribeUUIDDummyMethodForDisconnectCallback(string receivedMessage)
+        {
+            unsubscribeUUIDManualEvent.Set();
+        }
+
+        void DummyErrorCallback(string result)
+        {
+			Debug.Log("WhenAClientIsPresented ErrorCallback : " + result);
+        }
     }
 }
